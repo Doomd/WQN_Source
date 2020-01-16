200eec9e2747f687c6656f2cc8f8ce8a7a66f902
Doomd <bcosteloe@gmail.com>
2020-01-15 17:55:31 -0700

    Updates

---
+++ precache-manifest.2535335fa7fd22b743926f79fe0f65fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8067b14869cfb2e04c72ea60a2462791",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc31fa7cf39d581b7787",
+    "url": "/static/css/2.765f46be.chunk.css"
+  },
+  {
+    "revision": "ec62f47ec31b9b4cedab",
+    "url": "/static/css/main.a3781c2b.chunk.css"
+  },
+  {
+    "revision": "dc31fa7cf39d581b7787",
+    "url": "/static/js/2.64aac2c0.chunk.js"
+  },
+  {
+    "revision": "68aed49bcdcbc067487a3d461801ff7e",
+    "url": "/static/js/2.64aac2c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec62f47ec31b9b4cedab",
+    "url": "/static/js/main.957542b2.chunk.js"
+  },
+  {
+    "revision": "3c1ca9f169eaa3f05c24",
+    "url": "/static/js/runtime-main.5f4ee095.js"
+  },
+  {
+    "revision": "c38c0b47550e8ef5db2c5e71092ad362",
+    "url": "/static/media/HKGrotesk-Bold.c38c0b47.woff2"
+  },
+  {
+    "revision": "58192ae9b701ff8512cb5a9d8047543f",
+    "url": "/static/media/we-quote-nevada-logo-pink.58192ae9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WQN_Source2/precache-manifest.b2a940eb696c5c91e94d3960051a772b.js"
+  "/precache-manifest.2535335fa7fd22b743926f79fe0f65fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WQN_Source2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
